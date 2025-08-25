4c66d918fbf839a83bd95735fb67cd974c2c5b1b
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-08-25 12:24:32 +0600

    Home page design with js

---
+++ script.js
@@ -0,0 +1,17 @@
+//Log in button functionally
+document.getElementById("loginButton").
+addEventListener("click", function(e){
+    e.preventDefault();
+    const mobileNumber = 123456789
+    const pinNumber = 1234
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href = "./home.html"
+    }else{
+        alert("Invalid credentials. Please try again.")
+    }
+})
